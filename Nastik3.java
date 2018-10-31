9677fd0ff2b2230d87429605f71111f3b901a29b
NastyaSavina <sadfiz@mail.ru>
2018-10-31 20:20:13 +0300

    "31/10"

---
+++ Nastik3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Nastik3
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double r = in.nextDouble();
+        double R = in.nextDouble();
+        double kr1,kr2,kl;
+        kr1=3.1415*r*r;
+        kr2=3.1415*R*R;
+        kl=kr2-kr1;        
+        System.out.println("ploshad kolca: "+kl);
+        
+        in.close();
+    }
+}
