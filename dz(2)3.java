5069f5ebb29d62b620ed94faeb055b18a88b144d
NastyaSavina <sadfiz@mail.ru>
2018-11-02 15:59:55 +0300

    "2/11"

---
+++ dz(2)3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class N3
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        char ch;
+        System.out.println("введите букву: ");
+        ch = in.next().charAt(0);
+        switch (ch) {
+            case 'a':
+            System.out.println("ваша буква гласная ");
+                break;
+            case 'e':
+            System.out.println("ваша буква гласная ");
+                break;
+            case 'i':
+            System.out.println("ваша буква гласная "); 
+                break;
+            case 'o':
+            System.out.println("ваша  буква гласная  ");   
+                break;
+            case 'u':
+            System.out.println("ваша буква гласная  ");   
+                break;
+            default:
+            System.out.println("ваша буква негласная  ");
+                break;
+        }
+    }
+}
